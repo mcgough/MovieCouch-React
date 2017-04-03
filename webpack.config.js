a110bb8b0ad81446b5b4c6dea7062b939efd8a35
Dan McGough <mcgough.dan@gmail.com>
2017-04-02 23:34:12 -0700

    heroku debugging

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,6 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
 	template: './client/index.html',
-	filename: 'index.html',
 	inject: 'body'
 });
 
