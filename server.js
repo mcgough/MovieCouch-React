2834f13245deb77e18c0dad94491f57e3f19ba91
Dan McGough <mcgough.dan@gmail.com>
2017-04-02 23:11:06 -0700

    express

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
 
-app.use(express.static(__dirname + '/client'));
+app.use(express.static(__dirname + '/client/index.html'));
 
 app.listen(process.env.PORT || 8080);
