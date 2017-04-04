1c56308e5546e33244f7c4f3f48303ebfc371792
Dan McGough <mcgough.dan@gmail.com>
2017-04-03 22:23:12 -0700

    heroku debugging

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 var express = require('express');
 var app = express();
 
-app.use(express.static(__dirname + '/'));
+app.use(express.static(__dirname + '/client'));
 
 app.get('/', function(res,res) {
-	res.render('/client/index.html');
+	res.render('index.html');
 });
 
 app.listen(process.env.PORT || 8080);
