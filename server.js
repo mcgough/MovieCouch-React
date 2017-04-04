e603532984b49213ec8279e819b65e3f9b1dac4f
Dan McGough <mcgough.dan@gmail.com>
2017-04-03 21:25:21 -0700

    heroku debugging

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 
-app.use(express.static(__dirname + '/client'));
+// app.use(express.static(__dirname + '/client'));
 
 app.get('/', function(res,res) {
 	res.render('index.html');
