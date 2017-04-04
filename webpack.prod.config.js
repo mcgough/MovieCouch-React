a411ecae9f5654a4841c7698f721251e7b351e02
Dan McGough <mcgough.dan@gmail.com>
2017-04-03 19:16:48 -0700

    heroku debugging

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -9,7 +9,7 @@ console.log('###########!!!!!!!!!############');
 module.exports = {
 	entry: './client/index.js',
 	output: {
-		path: '/client/dist',
+		path: './client/dist',
 		filename: 'index_bundles.js'
 	},
 	module: {
