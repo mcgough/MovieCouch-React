879669699ea163ed9e1a6da8c1935a9bf0268238
Dan McGough <mcgough.dan@gmail.com>
2017-04-03 18:44:07 -0700

    heroku debugging

---
+++ webpack.prod.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+	template: './client/index.html',
+	inject: 'body'
+});
+console.log('###########!!!!!!!!!############');
+module.exports = {
+	entry: './client/index.js',
+	output: {
+		path: path.resolve('dist'),
+		filename: 'index_bundles.js'
+	},
+	module: {
+		loaders: [
+			{test: /\.js$/, loader: 'babel-loader', exclude: '/node_modules/'},
+			{test: /\.jsx$/, loader: 'babel-loader', exclude: '/node_modules/'}
+		]
+	}
+};
